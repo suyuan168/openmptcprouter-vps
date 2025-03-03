e3180c34ca5941af551019505fe512b9f06f5556
suyuan <175338101@qq.com>
2025-03-03 20:59:10 +0800

    Update debian9-x86_64.sh

--- debian9-x86_64.sh
+++ debian9-x86_64.sh
@@ -1791,17 +1791,13 @@ if [ "$GLORYTUN_TCP" = "yes" ]; then
 		rm -rf /tmp/glorytun-0.0.35
 		cd /tmp
 		if [ "$KERNEL" != "5.4" ]; then
-			# 非 5.4 内核使用 GitHub 克隆 tcp 分支的源码到 /tmp/glorytun-0.0.35
-			git clone https://github.com/suyuan168/glorytunserver.git glorytun-0.0.35
+			wget -O /tmp/glorytun-0.0.35.tar.gz https://github.com/Ysurac/glorytun/archive/refs/heads/tcp.tar.gz
 		else
-			# 5.4 内核下载发布版压缩包，并解压到 /tmp/glorytun-0.0.35
-			wget -O /tmp/glorytun-0.0.35.tar.gz https://hub.55860.com/angt/glorytun/releases/download/v0.0.35/glorytun-0.0.35.tar.gz
-			tar xzf glorytun-0.0.35.tar.gz
-			# 假设解压后目录名称与压缩包文件名不一致，则重命名为 glorytun-0.0.35
-			if [ ! -d "/tmp/glorytun-0.0.35" ]; then
-				extracted_dir=$(tar tzf glorytun-0.0.35.tar.gz | head -1 | cut -f1 -d"/")
-				mv "/tmp/${extracted_dir}" "/tmp/glorytun-0.0.35"
-			fi
+			wget -O /tmp/glorytun-0.0.35.tar.gz https://github.com/angt/glorytun/releases/download/v0.0.35/glorytun-0.0.35.tar.gz
+		fi
+		tar xzf glorytun-0.0.35.tar.gz
+		if [ "$KERNEL" != "5.4" ]; then
+			mv /tmp/glorytun-tcp /tmp/glorytun-0.0.35
 		fi
 		cd glorytun-0.0.35
 		./autogen.sh
